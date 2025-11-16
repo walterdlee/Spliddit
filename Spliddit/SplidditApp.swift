5abf70d15583cde5da83d81329bf33ee2a8e5539
Walter Lee <walter.3.lee@gmail.com>
2025-11-15 16:42:33 -0800

    Initial Commit

---
+++ Spliddit/SplidditApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplidditApp.swift
+//  Spliddit
+//
+//  Created by Walter Lee on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SplidditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
